3ae5eb37d830b19cf6d7be441e41b6581eeeeadf
Kellan-Stempin <stempinkellan@gmail.com>
2026-04-21 11:41:02 -0600

    np roll bounds fixed

--- life.py
+++ life.py
@@ -4,11 +4,15 @@ from components import Component
 from patterns import GOSPER_GUN
 
 def step(grid):
-    neighbors = sum(
-        np.roll(np.roll(grid, i, 0), j, 1)
-        for i in (-1, 0, 1)
-        for j in (-1, 0, 1)
-        if not (i == 0 and j == 0)
+    rows, cols = grid.shape
+
+    padded = np.pad(grid, pad_width=1, mode='constant', constant_values=0)
+
+    neighbors = (
+        padded[0:rows,   0:cols]   + padded[0:rows,   1:cols+1] + padded[0:rows,   2:cols+2] +
+        padded[1:rows+1, 0:cols]                                + padded[1:rows+1, 2:cols+2] +
+        padded[2:rows+2, 0:cols]   + padded[2:rows+2, 1:cols+1] + padded[2:rows+2, 2:cols+2]
+
     )
 
     return ((neighbors == 3) | ((grid == 1) & (neighbors == 2))).astype(np.uint8)
@@ -18,6 +22,7 @@ def place_pattern(grid, pattern, top=0, left=0):
         grid[top + r, left + c] = 1
 
 grid = np.zeros((500, 500), dtype=np.uint8)
+#grid = np.random.randint(0, 2, (200, 200), dtype=np.uint8)
 place_pattern(grid, GOSPER_GUN, top=10, left=10)
 
 plt.ion()
